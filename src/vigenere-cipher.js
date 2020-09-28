d286642d8bb2bcfc6ddc9cf0785a57a61e022b59
Andrei Lisovskiy <lis412@gmail.com>
2020-09-28 23:40:59 +0300

    feat: implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(forward = true) {
+    this.forward = forward;
+  }
+
+  processMessage(message, key, charFunction) {
+    if (typeof message === "undefined" || typeof key === "undefined")
+      throw new Error();
+
+    let keyIndex = 0;
+    let res = [];
+    for (let char of message) {
+      if (/[a-zA-Z]/.test(char)) {
+        res.push(
+          charFunction(
+            char.toUpperCase().charCodeAt(0),
+            key[keyIndex++ % key.length].toUpperCase().charCodeAt(0)
+          )
+        );
+      } else {
+        res.push(char);
+      }
+    }
+
+    if (!this.forward) res = res.reverse();
+    return res.join("");
+  }
+
+  encrypt(message, key) {
+    return this.processMessage(message, key, (charCode, keyCode) =>
+      String.fromCharCode(((charCode + keyCode) % 26) + 65)
+    );
+  }
+
+  decrypt(message, key) {
+    return this.processMessage(message, key, (charCode, keyCode) =>
+      String.fromCharCode(((charCode + 26 - keyCode) % 26) + 65)
+    );
   }
 }
 
