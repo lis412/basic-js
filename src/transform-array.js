f357cf87f8c82a7fb3f2c38dace5da679cb12715
lis412 <lis412@gmail.com>
2020-09-28 18:24:34 +0300

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  return arr.reduce((res, value, index, arr) => {
+    switch (value) {
+      case '--discard-next':
+      case '--discard-prev':
+      case '--double-next':
+      case '--double-prev':
+        // do nothing
+        break;    
+      default:
+        let cnt = 1;
+        if (arr[index - 1] === '--discard-next') break; //если предыдущая каманда "удалить", то больше ничего делать не надо
+        if (arr[index - 1] === '--double-next') cnt++;
+        if (arr[index + 1] === '--discard-prev') cnt--;
+        if (arr[index + 1] === '--double-prev') cnt++;
+        while (cnt-- > 0) res.push(value);
+    }
+    return res;
+  }, []);
 };
